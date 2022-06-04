95e7119acab664535bd4ade295a9cb8a6126c6e6
codingLogan <reachCodingLogan@gmail.com>
2022-06-03 20:29:41 -0600

    Add a template file

---
+++ _template.js
@@ -0,0 +1,14 @@
+const assert = require("node:assert");
+
+function algorithm() {
+  // Implement algorithm
+  return true;
+}
+
+function test() {
+  // Update tests
+  const expected = true;
+  assert.equal(algorithm(), expected);
+}
+
+test();
